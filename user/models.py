086c47fff04cb027a8779597c01cafe110fdf396
Василий <kochanvasia@mail.ru>
2022-12-09 15:30:53 +0300

    Добавлен просмотр аккаунт пользователя (осн. инф.), пользователь может указывать свой баланс при создании аккаунта

--- user/models.py
+++ user/models.py
@@ -10,7 +10,7 @@ class User(AbstractUser):
     fio = models.TextField(max_length=100, null=False, blank=False, verbose_name='ФИО')
     account_balance = models.DecimalField(default=0, decimal_places=2, max_digits=20, verbose_name='Баланс счёта')
 
-    REQUIRED_FIELDS = ['fio', 'email']
+    REQUIRED_FIELDS = ['fio', 'email', 'account_balance']
 
     class Meta:
         verbose_name = 'Пользователь'
